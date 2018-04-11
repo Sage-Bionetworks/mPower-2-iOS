b7d97efde1fd6b2ed52d07c25db9a4bc5fae8689
Shannon Young <shannon.young@sagebase.org>
2018-04-11 16:12:28 -0700

    Update JSON to use transformers

--- mPower2/MotorControlTests/NavigationTests.swift
+++ mPower2/MotorControlTests/NavigationTests.swift
@@ -2,6 +2,8 @@
 //  NavigationTests.swift
 //  MotorControlTests
 //
+//  Copyright © 2018 Sage Bionetworks. All rights reserved.
+//
 // Redistribution and use in source and binary forms, with or without modification,
 // are permitted provided that the following conditions are met:
 //
