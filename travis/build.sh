db55f1f643114e65f8541b748cbbcc0a2c771a0b
Shannon Young <shannon.young@sagebase.org>
2018-04-24 11:26:12 -0700

    Update travis build

--- travis/build.sh
+++ travis/build.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 set -ex
 if [[ "$TRAVIS_PULL_REQUEST" != "false" ]]; then     # on pull requests
-    echo "Testings is disabled because Travis does not support secrets on PR builds"
+    echo "Run unit tests against mPower2TestApp w/o the pointer to a private repo."
+    FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"mPower2/mPower2.xcodeproj" scheme:"mPower2TestApp"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag commits to master branch
     echo "Build on merge to master"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
