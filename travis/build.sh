232c6edde59aa1243c3b6ea3716379e7ac663088
Shannon Young <shannon.young@sagebase.org>
2018-04-24 12:21:06 -0700

    Fix schema casing for travis build

--- travis/build.sh
+++ travis/build.sh
@@ -7,11 +7,11 @@ elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" == "master" ]]; then  # non-tag com
     echo "Build on merge to master"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"mPower2/mPower2.xcodeproj" scheme:"mPower2"
-    bundle exec fastlane ci_archive scheme:"mpower2" export_method:"app-store" project:"mPower2/mPower2.xcodeproj"
+    bundle exec fastlane ci_archive scheme:"mPower2" export_method:"app-store" project:"mPower2/mPower2.xcodeproj"
 elif [[ -z "$TRAVIS_TAG" && "$TRAVIS_BRANCH" =~ ^stable-.* ]]; then # non-tag commits to stable branches
     echo "Build on stable branch"
     git clone https://github.com/Sage-Bionetworks/iOSPrivateProjectInfo.git ../iOSPrivateProjectInfo
     FASTLANE_EXPLICIT_OPEN_SIMULATOR=2 bundle exec fastlane test project:"mPower2/mPower2.xcodeproj" scheme:"mPower2"
-    bundle exec fastlane beta scheme:"mpower2" export_method:"app-store" project:"mPower2/mPower2.xcodeproj"
+    bundle exec fastlane beta scheme:"mPower2" export_method:"app-store" project:"mPower2/mPower2.xcodeproj"
 fi
 exit $?
