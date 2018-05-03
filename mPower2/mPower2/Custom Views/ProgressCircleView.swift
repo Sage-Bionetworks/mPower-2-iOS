82d872306024a35301e8a667430c150bb2555057
Josh Bruhin <josh@saviapps.com>
2018-05-02 18:31:58 -0600

    Initial work on main Tracking tab

---
+++ mPower2/mPower2/Custom Views/ProgressCircleView.swift
@@ -0,0 +1,21 @@
+//
+//  ProgressCircleView.swift
+//  mPower2
+//
+//  Created by Josh Bruhin on 4/30/18.
+//  Copyright © 2018 Sage Bionetworks. All rights reserved.
+//
+
+import UIKit
+
+class ProgressCircleView: UIView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
---
+++ mPower2/mPower2/View Controllers/TaskBrowserViewController.swift
@@ -0,0 +1,35 @@
+//
+//  TaskBrowserViewController.swift
+//  mPower2
+//
+//  Created by Josh Bruhin on 5/1/18.
+//  Copyright © 2018 Sage Bionetworks. All rights reserved.
+//
+
+import UIKit
+
+class TaskBrowserViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
