3a37f493c18ea8e044690e8a784130ed298f52b6
syoung-smallwisdom <shannon.young@sagebase.org>
2019-04-08 23:06:23 -0700

    Update override to set the color palette

--- mPower2/mPower2/AppDelegate.swift
+++ mPower2/mPower2/AppDelegate.swift
@@ -49,7 +49,7 @@ class AppDelegate: SBAAppDelegate, RSDTaskViewControllerDelegate {
         return MP2BridgeConfiguration()
     }
     
-    override func instantiateColorPalette() -> RSDColorPalette {
+    override func instantiateColorPalette() -> RSDColorPalette? {
         let primary = RSDColorMatrix.shared.colorKey(for: .palette(.royal), version: 0, index: 3)
         let secondary = RSDColorMatrix.shared.colorKey(for: .palette(.butterscotch), version: 0, index: 3)
         return RSDColorPalette(version: 0, primary: primary, secondary: secondary, accent: primary)
