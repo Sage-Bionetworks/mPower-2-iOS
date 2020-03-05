741e8a6dc7281c2c10161492a7031ab4ad1886e5
Shannon Young <shannon.young@sagebase.org>
2020-03-05 11:55:01 -0800

    When a notification for a study burst fires, schedule repeat at 90 days

--- mPower2/mPower2/StudyBurstScheduleManager.swift
+++ mPower2/mPower2/StudyBurstScheduleManager.swift
@@ -852,6 +852,17 @@ class StudyBurstScheduleManager : TaskGroupScheduleManager {
         return (requests, pendingRequestIds)
     }
     
+    func willPresentNotification(_ notification: UNNotification) {
+        let nextDate = notification.date.addingNumberOfDays(90)
+        let dateComponents = Calendar.iso8601.dateComponents([.year, .month, .day, .hour, .minute],
+                                                             from: nextDate)
+        let trigger = UNCalendarNotificationTrigger(dateMatching: dateComponents, repeats: false)
+        let nextRequest = UNNotificationRequest(identifier: notification.request.identifier,
+                                                content: notification.request.content,
+                                                trigger: trigger)
+        UNUserNotificationCenter.current().add(nextRequest)
+    }
+    
     func getLocalNotificationIdentifier(for schedule: SBBScheduledActivity, at time: DateComponents) -> String {
         let timeIdentifier = time.jsonObject()
         return "\(schedule.guid) \(timeIdentifier)"
